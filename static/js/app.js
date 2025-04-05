aa96432e624d2ba6d8c604d1df887e3aa8db119d
geraldine1456 <gcvalencia1456@yahoo.com>
2025-04-05 08:06:48 -0400

    updated apps.js v2

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,14 @@
 // Build the metadata panel
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-    console.log("Metadata:", data.metadata);
+    console.log("Metadata:", data.metadata);  //to check the metadata field
 
     // get the metadata field
     let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
     let filteredMetadata = metadata.filter(obj => obj.id == sample)[0];
-    console.log("Filtered Metadata:", filteredMetadata);
+    console.log("Filtered Metadata:", filteredMetadata); //to check the filtered metadata
 
     // Use d3 to select the panel with id of `#sample-metadata`
     let panel = d3.select("#sample-metadata");
@@ -16,8 +16,7 @@ function buildMetadata(sample) {
     // Use `.html("") to clear any existing metadata
     panel.html("");
 
-    // Inside a loop, you will need to use d3 to append new
-    // tags for each key-value in the filtered metadata.
+    // Inside a loop, use d3 to append new tags for each key-value in the filtered metadata.
     Object.entries(filteredMetadata).forEach(([key, value]) => {
       panel.append("h5").text(`${key}: ${value}`);
     });
@@ -28,14 +27,14 @@ function buildMetadata(sample) {
 // function to build both charts
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-    console.log("Samples Data:", data.samples);
+    console.log("Samples Data:", data.samples); //to check the samples field
    
     // Get the samples field
     let samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
     let filteredSample = samples.filter(obj => obj.id == sample)[0];
-    console.log("Filtered Sample:", filteredSample);
+    console.log("Filtered Sample:", filteredSample); //to check the filtered sample
 
     // Get the otu_ids, otu_labels, and sample_values
     let otu_ids = filteredSample.otu_ids;
@@ -68,8 +67,7 @@ function buildCharts(sample) {
     // For the Bar Chart, map the otu_ids to a list of strings for yticks
     let yticks = otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
    
-    // Build a Bar Chart
-    // slice and reverse the input data appropriately
+    // Build a Bar Chart, slice and reverse the input data appropriately
   
     let barTrace = {
       x: sample_values.slice(0, 10).reverse(),
@@ -94,7 +92,7 @@ function buildCharts(sample) {
 // Function to run on page load
 function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-    console.log("Dataset Names:", data.names);
+    console.log("Dataset Names:", data.names); //to check the names field
    
     // Get the names field
     let sampleNames = data.names;
@@ -103,15 +101,14 @@ function init() {
     let dropdown = d3.select("#selDataset");
 
     // Use the list of sample names to populate the select options
-    // Hint: Inside a loop, you will need to use d3 to append a new
-    // option for each sample name.
+    // use d3 to append a new option for each sample name.
     sampleNames.forEach((name) => {
       dropdown.append("option").text(name).property("value", name);
     });
 
     // Get the first sample from the list
     let firstSample = sampleNames[0];
-    console.log("First Sample:", firstSample);
+    console.log("First Sample:", firstSample); //- to check the first sample
 
     // Build charts and metadata panel with the first sample
     buildMetadata(firstSample);
@@ -122,7 +119,7 @@ function init() {
 
 // Function for event listener
 function optionChanged(newSample) {
-  console.log("New Sample Selected:", newSample);
+  console.log("New Sample Selected:", newSample); //to check the new sample selected
  
 // Build charts and metadata panel each time a new sample is selected
 buildMetadata(newSample);
